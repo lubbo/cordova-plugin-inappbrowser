1840deb7a390e3de4f32bfd5321d773c6620206d
Davide Ramo <davide.ramo.ve@gmail.com>
2023-11-02 16:01:24 +0100

    feat: iOS title and close button

--- src/ios/CDVWKInAppBrowser.h
+++ src/ios/CDVWKInAppBrowser.h
@@ -59,6 +59,7 @@
 @property (nonatomic, strong) IBOutlet WKWebViewConfiguration* configuration;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* closeButton;
 @property (nonatomic, strong) IBOutlet UILabel* addressLabel;
+@property (nonatomic, strong) IBOutlet UILabel* titleLabel;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* backButton;
 @property (nonatomic, strong) IBOutlet UIBarButtonItem* forwardButton;
 @property (nonatomic, strong) IBOutlet UIActivityIndicatorView* spinner;
@@ -72,8 +73,8 @@
 - (void)close;
 - (void)navigateTo:(NSURL*)url options:(CDVInAppBrowserOptions*)options;
 - (void)showLocationBar:(BOOL)show;
-- (void)showToolBar:(BOOL)show : (NSString *) toolbarPosition;
-- (void)setCloseButtonTitle:(NSString*)title : (NSString*) colorString : (int) buttonIndex;
+- (void)showToolBar:(BOOL)show
+                   :(NSString *) toolbarPosition;
 
 - (id)initWithBrowserOptions: (CDVInAppBrowserOptions*) browserOptions andSettings:(NSDictionary*) settings;
 
